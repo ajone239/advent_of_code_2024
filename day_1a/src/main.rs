02ef65da081f0eb083b50f9716ac62467c3e90dd
Austin Jones <ajones53.aj@gmail.com>
2024-12-01 22:31:36 -0500

    day 1a first attempt

--- day_1a/src/main.rs
+++ day_1a/src/main.rs
@@ -1,6 +1,33 @@
 use std::io;
 
-fn main() -> io::Result<()> {
-    println!("Hello");
+use anyhow::Result;
+
+fn main() -> Result<()> {
+    let stdin = io::stdin();
+
+    let mut left = vec![];
+    let mut rite = vec![];
+
+    for line in stdin.lines() {
+        let line = line.unwrap();
+
+        let mut vals = line.split_whitespace().map(|s| s.parse::<i32>().unwrap());
+
+        left.push(vals.next().unwrap());
+        rite.push(vals.next().unwrap());
+    }
+
+    left.sort();
+    rite.sort();
+
+    let difference: i32 = left
+        .into_iter()
+        .zip(rite.into_iter())
+        .map(|(l, r)| r - l)
+        .map(|d| d.abs())
+        .sum();
+
+    println!("{}", difference);
+
     Ok(())
 }

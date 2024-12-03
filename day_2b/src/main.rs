4bbb22eec8b4fc774e8ea97ad18da68fa60b5bcb
Austin Jones <ajones53.aj@gmail.com>
2024-12-02 22:01:18 -0500

    2b works

--- day_2b/src/main.rs
+++ day_2b/src/main.rs
@@ -17,23 +17,72 @@ fn main() -> Result<()> {
         reports.push(report);
     }
 
-    let count = reports
-        .into_iter()
-        .map(|report| {
-            report
-                .windows(2)
-                .map(|arr| arr[0] - arr[1])
-                .collect::<Vec<_>>()
-        })
-        // check for monotonic increase or decrease
-        .filter(|diffs| diffs.iter().all(|v| v > &0) || diffs.iter().all(|v| v < &0))
-        // no change greater than 3
-        .filter(|diffs| diffs.iter().map(|v| v.abs()).max().unwrap() <= 3)
-        // no change smaller than 1
-        .filter(|diffs| diffs.iter().map(|v| v.abs()).min().unwrap() >= 1)
-        .count();
+    let mut safe_count = 0;
+    for (i, mut report) in reports.into_iter().enumerate() {
+        let errors = find_error_events(&report);
 
-    println!("{}", count);
+        println!("{:?}", i);
+        println!("{:?}", report);
+        println!("{:?}", errors);
+        println!();
+
+        let event_to_remove = match errors.len() {
+            0 => {
+                safe_count += 1;
+                continue;
+            }
+            len => errors[len - 1],
+        };
+
+        let val = report.remove(event_to_remove);
+
+        let errors = find_error_events(&report);
+
+        if errors.is_empty() {
+            safe_count += 1;
+            continue;
+        }
+
+        report.insert(event_to_remove, val);
+
+        report.remove(event_to_remove + 1);
+
+        let errors = find_error_events(&report);
+
+        if errors.is_empty() {
+            safe_count += 1;
+            continue;
+        }
+
+        println!("{:?}", report);
+        println!("{:?}", errors);
+        println!();
+        println!();
+    }
+
+    println!("{}", safe_count);
 
     Ok(())
+}
+
+fn find_error_events(report: &Vec<i32>) -> Vec<usize> {
+    let diffs: Vec<(usize, i32)> = report
+        .windows(2)
+        .enumerate()
+        .map(|(i, arr)| (i, arr[0] - arr[1]))
+        .collect();
+
+    let pos_count = diffs.iter().filter(|(_, v)| *v > 0).count();
+    let neg_count = diffs.iter().filter(|(_, v)| *v < 0).count();
+
+    let pon = pos_count > neg_count;
+
+    diffs
+        .into_iter()
+        .filter(|(_, diff)| {
+            let val = diff.abs();
+            val < 1 || val > 3 || (*diff > 0) != pon
+        })
+        .map(|(i, _)| i)
+        .collect()
 }

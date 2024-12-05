c95cdd4e2079ee647c98cdd087b88d2876008739
Austin Jones <ajones53.aj@gmail.com>
2024-12-05 08:50:58 -0500

    get stuff read in

--- day_5a/src/main.rs
+++ day_5a/src/main.rs
@@ -5,80 +5,47 @@ use anyhow::Result;
 fn main() -> Result<()> {
     let stdin = io::stdin();
 
-    let mut puzzle: Vec<Vec<char>> = vec![];
+    let mut orderings: Vec<String> = vec![];
+    let mut updates: Vec<String> = vec![];
 
+    let mut oou = true;
     for line in stdin.lines() {
         let line = line?;
 
-        let row = line.chars().collect();
+        if line.is_empty() {
+            oou = false;
+            continue;
+        }
 
-        puzzle.push(row);
-    }
-
-    /* The shape of the mask made in our checks
-     *
-     *     ****
-     *    ***
-     *   * * *
-     *  *  *  *
-     */
-    let directions = [(0, 1), (1, 0), (-1, 1), (1, 1)];
-
-    let cross_word = CrossWord::new(puzzle);
-
-    let mut count = 0;
-
-    for i in 0..cross_word.length {
-        for j in 0..cross_word.width {
-            for dir in directions {
-                let i = i as isize;
-                let j = j as isize;
-
-                let word: String = (0..4)
-                    .map(|k| (i + k * dir.0, j + k * dir.1))
-                    .map(|(i, j)| cross_word.bounded_at(i, j))
-                    .filter(|c| c.is_some())
-                    .map(|c| c.unwrap())
-                    .collect();
-
-                if word == "XMAS" || word == "SAMX" {
-                    count += 1;
-                }
-            }
+        if oou {
+            orderings.push(line);
+        } else {
+            updates.push(line)
         }
     }
 
-    println!("{}", count);
+    let orderings: Vec<(usize, usize)> = orderings
+        .into_iter()
+        .map(|s| {
+            s.split('|')
+                .map(|sp| sp.parse::<usize>().unwrap())
+                .collect()
+        })
+        .map(|i: Vec<usize>| (i[0], i[1]))
+        .collect();
+
+    let updates: Vec<Vec<usize>> = updates
+        .into_iter()
+        .map(|s| {
+            s.split(',')
+                .map(|sp| sp.parse::<usize>().unwrap())
+                .collect()
+        })
+        .collect();
+
+    println!("{:?}", orderings);
+    println!();
+    println!("{:?}", updates);
 
     Ok(())
-}
-
-struct CrossWord {
-    length: usize,
-    width: usize,
-    puzzle: Vec<Vec<char>>,
-}
-
-impl CrossWord {
-    fn new(puzzle: Vec<Vec<char>>) -> Self {
-        let length = puzzle.len();
-        let width = puzzle[0].len();
-
-        Self {
-            length,
-            width,
-            puzzle,
-        }
-    }
-
-    fn bounded_at(&self, i: isize, j: isize) -> Option<char> {
-        if i < 0 || j < 0 || i >= self.length as isize || j >= self.width as isize {
-            return None;
-        }
-
-        let i = i as usize;
-        let j = j as usize;
-
-        Some(self.puzzle[i][j])
-    }
 }
